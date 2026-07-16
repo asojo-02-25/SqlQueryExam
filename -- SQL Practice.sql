1f6ab5f9d4528e88696a0160680b40d5f4bd4950
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-07-16 11:40:21 +0900

    前日復習完了

--- -- SQL Practice.sql
+++ -- SQL Practice.sql
@@ -55,7 +55,7 @@ WHERE Address LIKE '兵庫県%' AND ShortName LIKE '%神戸%';
 
 SELECT DISTINCT CompanyID FROM dbo.tbl_Branch;
 
--- 4.4 EstablishDateが新しい順に10件取得
+-- 4.4 OFFSET - FETCH
 
 SELECT * FROM dbo.tbl_Branch ORDER BY EstablishDate DESC OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY;
 
@@ -91,7 +91,102 @@ SELECT *,
     END AS 年度管理
 FROM dbo.tbl_Branch
 
--- 5.3 関数
+-- 5.3 文字列に関する関数
 
-SELECT Name, NameKana, Length(Name) AS 名前の長さ, Length(NamaKana) AS 読み仮名の長さ
+-- LEN
+
+SELECT Name, NameKana, LEN(Name) AS 名前の長さ, LEN(NameKana) AS 読み仮名の長さ
+FROM dbo.tbl_Branch;
+
+-- REPLACE
+
+BEGIN TRANSACTION;
+
+UPDATE dbo.tbl_Branch SET Phone1 = REPLACE(Phone1, 1000000000, '100-000-0000')
+
+SELECT * FROM dbo.tbl_Branch;
+
+ROLLBACK TRANSACTION;
+
+-- SUBSTRING
+
+SELECT BranchID, Address FROM dbo.tbl_Branch
+WHERE SUBSTRING(Address, 1, 3) = '兵庫県';
+
+-- CONCAT 
+
+SELECT CONCAT(BranchCode, ':', Name)  AS '支店コード : 名前' FROM dbo.tbl_Branch ORDER BY BranchCode;
+
+-- 5.5 数値に関する関数
+
+-- ROUND
+
+SELECT ZipCode, ROUND(CAST(ZipCode AS int), -6) AS '四捨五入した郵便番号' FROM dbo.tbl_Branch;
+
+SELECT ZipCode, ROUND(CAST(ZipCode AS int), -6, 1) AS '切り捨てた郵便番号' FROM dbo.tbl_Branch;
+
+SELECT ZipCode, POWER(ZipCode, 10) AS '郵便番号の10乗' FROM dbo.tbl_Branch;
+
+-- 5.6 日付に関する関数
+
+BEGIN TRANSACTION;
+
+INSERT INTO dbo.tbl_Branch (BranchID, EstablishDate) VALUES (295, CURRENT_TIMESTAMP);
+
+SELECT * FROM dbo.tbl_Branch;
+
+ROLLBACK TRANSACTION;
+
+-- 5.7 置換に関する関数
+
+SELECT ZipCode, COALESCE(ZipCode, '未設定') AS 編集済み郵便番号 FROM dbo.tbl_Branch; 
+
+-- 5.9 練習問題
+
+-- 5-2-1
+UPDATE 回答者 SET 国名 = CASE SUBSTRING(TRIM(メールアドレス), LEN(TRIM(メールアドレス))-1, 2)
+                        WHEN 'jp' THEN '日本'
+                        WHEN 'uk' THEN 'イギリス'
+                        WHEN 'cn' THEN '中国'
+                        WHEN 'fr' THEN 'フランス'
+                        WHEN 'vn' THEN 'ベトナム'
+                        END;
+
+-- 5-2-2 
+
+SELECT TRIM(メールアドレス), 
+CONCAT(
+CASE
+    WHEN 年齢 >= 20 AND 年齢 < 30 THEN '20代'
+    WHEN 年齢 >= 30 AND 年齢 < 40 THEN '30代'
+    WHEN 年齢 >= 40 AND 年齢 < 50 THEN '40代'
+    WHEN 年齢 >= 50 AND 年齢 < 60 THEN '50代' END, ':',
+
+CASE 住居
+    WHEN C THEN '集合住宅'
+    WHEN D THEN '戸建て' END AS 属性
+
+FROM 回答者
+
+-- 5-3-1
+UPDATE 受注 SET 文字数 = LEN(REPLACE(文字, ' ', ''))
+
+-- 5-3-2
+
+
+-- 6.1 集計関数
+
+SELECT SUM(LEN(Address)), AVG(LEN(Address)), MAX(LEN(Address)), MIN(LEN(Address))
+FROM dbo.tbl_Branch;
+
+SELECT COUNT(*) FROM dbo.tbl_Branch;
+SELECT COUNT(BranchID) FROM dbo.tbl_Branch;
+SELECT COUNT(ZipCode) FROM dbo.tbl_Branch;
+
+-- 6.2 nullの取り扱い
+
+SELECT SUM(LEN(FAX2)), AVG(LEN(Address)), MAX(LEN(Address)), MIN(LEN(Address))
+FROM dbo.tbl_Branch;
+
+SELECT SUM(COALESCE(FAX2, 0)), AVG(COALESCE(FAX2, 0)), MAX(COALESCE(FAX2, 0)), MIN(COALESCE(FAX2, 0))
 FROM dbo.tbl_Branch;
--- -- SQL Review.sql
+++ -- SQL Review.sql
@@ -167,16 +167,101 @@ SELECT * FROM 奇数テーブル;
 
 -- 2. 
 SELECT * FROM 整数テーブル
-EXCEPT ALL
+EXCEPT
 SELECT * FROM 偶数テーブル;
 
 
 -- 3. 
 SELECT * FROM 整数テーブル
-INTERSECT ALL
+INTERSECT
 SELECT * FROM 偶数テーブル;
 
 -- 4. 
 SELECT * FROM 偶数テーブル
-INTERSECT ALL
+INTERSECT
 SELECT * FROM 奇数テーブル;
+
+
+-- 5.9 練習問題
+
+-- 5-1
+
+-- 1. 
+UPDATE 試験結果 SET 午後1 = (80 * 4 - (86 + 68 + 91)) WHERE 受験者ID = 'SW1046';
+
+-- 2. 
+SELECT 受験者ID AS 合格者ID FROM 試験結果 WHERE 午前 >= 60 AND (午後1 + 午後2) >= 120 AND 論述 >= (午後 + 午後1 + 午後2) / 30;
+
+-- 5-2
+
+-- 1.
+UPDATE 回答者 SET 国名 = CASE SUBSTRING(メールアドレス, LEN(メールアドレス) -1, 2)
+    WHEN 'jp' THEN '日本'
+    WHEN 'uk' THEN 'イギリス'
+    WHEN 'cn' THEN '中国'
+    WHEN 'fr' THEN 'フランス'
+    WHEN 'vn' THEN 'ベトナム' END;
+
+-- 2. 
+SELECT TRIM(メールアドレス) AS メールアドレス, CONCAT(
+    ROUND(年齢, -1, 1), '代', ' : ', 
+    CASE 住居 
+    WHEN 'C' THEN '集合住宅'
+    WHEN 'D' THEN '戸建て' END) AS 属性 
+FROM 回答者 
+WHERE 年齢 BETWEEN 20 AND 59;
+
+ -- 5-3
+
+ -- 1. 
+UPDATE 受注テーブル SET 文字列 = LEN(REPLACE(文字, ' ', ''));
+
+-- 2. 
+SELECT 受注日, 受注ID, 文字数, 
+CASE 書体コード 
+    WHEN '1' THEN 'ブロック体'
+    WHEN '2' THEN '筆記体'
+    WHEN '3' THEN '草書体' END AS 書体名, 
+CASE 
+    WHEN 書体コード = '1' THEN 100 * LEN(REPLACE(文字, ' ', ''))
+    WHEN 書体コード = '2' THEN 150 * LEN(REPLACE(文字, ' ', ''))
+    WHEN 書体コード = '3' THEN 200 * LEN(REPLACE(文字, ' ', '')) END AS 単価, 
+CASE
+    WHEN LEN(REPLACE(文字, ' ', '')) >= 10 THEN 500
+    ELSE 0 END AS 特別加工料;
+
+-- 3. 
+UPDATE 受注 SET 文字 = REPLACE(TRIM(文字), ' ', '★') WHERE 受注ID = '113';
+
+-- 6.7 練習問題
+
+-- 6-1
+
+-- 1. 
+SELECT SUM(降水量), AVG(最高気温), AVG(最低気温) FROM 都市別気象観測;
+
+--2. 
+SELECT SUM(降水量), AVG(最高気温), AVG(最低気温) FROM 都市別気象観測 WHERE 都市名 = '東京';
+
+--3. 
+SELECT AVG(降水量), MIN(最高気温), MAX(最低気温) FROM 都市別気象観測 GROUP BY 都市名;
+
+-- 4. 
+SELECT AVG(降水量), AVG(最高気温), AVG(最低気温) FROM 都市別気象観測 GROUP BY 月;
+
+-- 5. 
+SELECT 都市名, MAX(最高気温) FROM 都市別気象観測 GROUP BY 都市名 HAVING 最高気温 >= 38;
+
+-- 6. 
+SELECT 都市名, MIN(最低気温) FROM 都市別気象観測 GROUP BY 都市名 HAVING 最低気温  <= -10;
+
+-- 6-2 
+
+-- 1. 
+SELECT COUNT(*) - COUNT(退室) AS 入室中 FROM 入退室管理;
+
+-- 2. 
+SELECT COUNT(社員名) AS 入室回数 FROM 入退室管理 GROUP BY 社員名 ORDER BY 1 DESC;
+
+-- 3.
+SELECT 
